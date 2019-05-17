e485563238423442233580ecf6734d2acb757c36
abbyreyna <reyna.abby08@gmail.com>
2019-05-16 18:15:25 -0700

    vote logic & added ids to vote button

--- public/js/votes.js
+++ public/js/votes.js
@@ -1,6 +1,40 @@
-$(document).on("click", function(){
-    $.ajax({
-        method: "GET",
-        url: "/"
+$(function () {
+    $(".upVote").on("click", function (event) {
+        var id = $(this).attr("id");
+
+        var uLike = {
+            likes: 1
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/author/" + id, {
+            type: "PUT",
+            data: uLike
+        }).then(
+            function () {
+                console.log("liked");
+                // Reload the page to get the updated list
+                location.reload();
+            });
+    });
+
+    $(".dwnVote").on("click", function (event) {
+        var id = $(this).attr("id");
+
+        var uDislike = {
+            dislikes: 1
+        };
+        // Send the PUT request.
+        $.ajax("/api/author/" + id, {
+            type: "PUT",
+            data: uDislike
+        }).then(
+            function () {
+                console.log("disliked");
+                // Reload the page to get the updated list
+                location.reload();
+            });
     })
-})
+
+    
+});
